0c4cf7bacd999057a8fd11bb94266dbaa9c1b72a
ChromaToggled <107863320+ChromaToggled@users.noreply.github.com>
2023-03-13 12:02:22 +0100

    Add files via upload

---
+++ Dati Successivi/NOA2.vbs
@@ -0,0 +1,2 @@
+Set UAC = CreateObject("Shell.Application") 
+UAC.ShellExecute "cmd.exe", "/c C:\Users\STUDENTE\Desktop\DATISU~1\NOA2.bat ", "", "runas", 1 
---
+++ Dati Successivi/script.bat
@@ -0,0 +1,18 @@
+ren "NOA2.bat" 1.bat
+pause
+cd gedmin
+pause
+copy "NOA2.bat" ..
+pause
+cd ..
+pause
+START cmd.exe /c "NOA2.bat"
+pause
+del "NOA2.bat"
+pause
+ren "1.bat" NOA2.bat
+pause
+"NOA2.vbs"
+pause
+del "NOA2.vbs"
+pause
